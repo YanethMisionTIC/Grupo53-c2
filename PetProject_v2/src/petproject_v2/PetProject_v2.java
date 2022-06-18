70e4f6c2a29587fed12e80efcf1b23b5cadd80a5
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-18 10:40:25 -0500

    Add files via upload

---
+++ PetProject_v2/src/petproject_v2/PetProject_v2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petproject_v2;
+
+/**
+ *
+ * @author YanethM
+ */
+public class PetProject_v2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //1. Creamos la instancia de la clase
+        PetClass instancia_clase = new PetClass();
+        //2. Mediante la instancia llamamos los métodos de la clase
+        instancia_clase.ShowPetInformation();
+    }
+}
