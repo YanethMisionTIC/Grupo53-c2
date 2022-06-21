3bd899e21ec8721b3432ab7e8f68606a62c44559
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-20 19:28:02 -0500

    Create Main.java

---
+++ RETOS/SEMANA 1/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice1.g53;
+
+/**
+ *
+ * @author YanethM
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //1. Creamos las instancias de las demás clases del paquete
+        Exercise1 exercise1Instance = new Exercise1();
+        
+        //2. Mediante la instancia lo creamos
+        exercise1Instance.RequestDataUser();
+    }
+}
