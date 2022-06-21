51e0c18d136a772aeca0787dd3833bb0d3264ccc
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-20 20:30:16 -0500

    Update Main.java

--- RETOS/SEMANA 1/Main.java
+++ RETOS/SEMANA 1/Main.java
@@ -5,6 +5,8 @@
  */
 package practice1.g53;
 
+import java.util.Scanner;
+
 /**
  *
  * @author YanethM
@@ -15,15 +17,54 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //1. Creamos las instancias de las demás clases del paquete
+        //1. Creamos la instancia de la librería Scanner
+        Scanner scannerInstance = new Scanner(System.in);
+        //2. Variables de opciones del menu
+        boolean exitMenu = false;
+        int menuOption = 0;
+
+        //3. Creamos las instancias de las demás clases del paquete
         Exercise1 exercise1Instance = new Exercise1();
         Exercise2 exercise2Instance = new Exercise2();
         Exercise3 exercise3Instance = new Exercise3();
         Exercise4 exercise4Instance = new Exercise4();
-        //2. Mediante la instancia lo creamos
-        exercise1Instance.RequestDataUser();
-        exercise2Instance.NumberOddEven();
-        exercise3Instance.NegativeNumberOddEven();
-        exercise4Instance.NegativeNumberOdd();
+        Exercise5 exercise5Instance = new Exercise5();
+
+        //3. Ciclo while del menu
+        // exitMenu = false
+        // !exitMenu = true
+        while (!exitMenu) {
+            System.out.println("            SELECCIONA UNA OPCIÓN DEL MENU");
+            System.out.println("1. Ejercicio 1\n2. Ejercicio 2\n3. Ejercicio 3\n4. Ejercicio 4\n5. Ejercicio 5\n6. Ejercicio 6\n7. Ejercicio 7\n8. Salir\n>>>");
+            menuOption = scannerInstance.nextInt();
+
+            switch (menuOption) {
+                case 1:
+                    exercise1Instance.RequestDataUser();
+                    break;
+                case 2:
+                    exercise2Instance.NumberOddEven();
+                    break;
+                case 3:
+                    exercise3Instance.NegativeNumberOddEven();
+                    break;
+                case 4:
+                    exercise4Instance.NegativeNumberOdd();
+                    break;
+                case 5:
+                    exercise5Instance.ValidateWifiPassword();
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    //Cuando la variable exitMenu toma este valor, deja de ingresar al while
+                    exitMenu = true;
+                    break;
+                default:
+                    System.out.println("Sólo se admiten opciones entre 1 y 8");
+            }
+        }
     }
 }
