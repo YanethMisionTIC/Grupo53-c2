d43a1183d0a40bd22eff326b94d6f7fbe747dea5
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-23 19:52:05 -0500

    Create Operator.java

---
+++ SEMANA 2/Sesion 2/Herencia/Operator.java
@@ -0,0 +1,13 @@
+
+package Herencia;
+
+
+public class Operator {
+    private int idOperator;
+    private static int operatorCounter;
+    public boolean handlingHeavyMachinery;
+    public int riskLevelArl;
+    
+    //1. Insertar constructor de la clase
+    //2. Insertar función que imprime el objeto de operador
+}
