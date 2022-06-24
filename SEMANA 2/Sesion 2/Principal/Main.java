3a404ef503fa4946e56bc354e98213e79a944d28
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-23 19:32:09 -0500

    Update Main.java

--- SEMANA 2/Sesion 2/Principal/Main.java
+++ SEMANA 2/Sesion 2/Principal/Main.java
@@ -6,24 +6,21 @@ import Herencia.*;
 public class Main {
 
     public static void main(String[] args) {
-        Person instancePerson1 = new Person("Marci", "Ana");
-        Person instancePerson2 = new Person("Marco", "de la Puerta");
-        Person instancePerson3 = new Person("Jorge", "Iván");
         /*
-            En Java un arreglo es semejante a las listas en python []
-                studentsList = ["Javier", "Jacinto", "Marci"]
-                arrayDeObjectos = 
-                    [
-                        Person1 = {nombre: "Yaneth", apellido: "Mejía"},
-                        Person2 = {nombre: "Luis", apellido: "Betancourt"}
-                    ]
+        =================================================================================
+        ==================================Employee====================================
+        =================================================================================
+        */
         
-            En Java los objetos tienen la misma estructura que los diccionarios en python {}
-                objeto = {clave: valor} 
-                person = {nombre: "Yaneth"}
-         */
-        System.out.println("ArrayPersonas:\n    [\n" + instancePerson1.toString()
-                + ",\n" + instancePerson2.toString() + ",\n" + instancePerson3.toString()
-                + "\n    ]");
+        String[] endowment1 = new String[]{"Guantes", "Casco"};
+        String[] endowment2 = new String[]{"Portatil", "Guaya"};
+
+        Employee instanceEmployee1 = new Employee(3400000, true, endowment2, "Tulio", "Mejía");
+        Employee instanceEmployee2 = new Employee(1200000, true, endowment1, "Ernesto", "Díaz");
+        Employee instanceEmployee3 = new Employee(7600000, true, endowment2, "Lola", "Mento");
+
+        System.out.println("ArrayPersonas:\n    [\n" + instanceEmployee1.toString()
+                + ",\n" + instanceEmployee2.toString() + ",\n"
+                + instanceEmployee3.toString() + "\n    ]");
     }
 }
