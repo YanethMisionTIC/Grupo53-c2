e085e5770851c15c8280d1bdd738017f409d874a
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-28 22:26:33 -0500

    Add files via upload

---
+++ SEMANA 3/Sesion 2/Principal/Main.java
@@ -0,0 +1,13 @@
+package Principal;
+import Vistas.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //1. Creamos la instancia de la vista login
+        Login login = new Login();
+        //El método setVisible hace visible la ventana
+        login.setVisible(true);
+    }
+    
+}
